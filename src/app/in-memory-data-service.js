51be7cb0d54c17018ded3f59458099cdc509d157
surabhishankar <sshan001@odu.edu>
2017-07-17 03:31:04 -0400

    Making changes to appearence

--- src/app/in-memory-data-service.js
+++ src/app/in-memory-data-service.js
@@ -4,17 +4,56 @@ var InMemoryDataService = (function () {
     }
     InMemoryDataService.prototype.createDb = function () {
         var cars = [
-            { id: 0, name: 'Zero', content: 'Test Content 0' },
-            { id: 1, name: 'Spiderman', content: 'Test Content 1' },
-            { id: 2, name: 'Batman', content: 'Test Content 2' },
-            { id: 3, name: 'Wolverine', content: 'Test Content 3' },
-            { id: 4, name: 'Ironman', content: 'Test Content 4' },
-            { id: 5, name: 'Flash', content: 'Test Content 5' },
-            { id: 6, name: 'Arrow', content: 'Test Content 6' },
-            { id: 7, name: 'Antman', content: 'Test Content 7' },
-            { id: 8, name: 'Avatar', content: 'Test Content 8' },
-            { id: 9, name: 'Deadpool', content: 'Test Content 9' },
-            { id: 10, name: 'Thor', content: 'Test Content 10' }
+            {
+                id: 1,
+                name: 'McLaren P1 LM',
+                content: 'Price: $3.7 million, Engine: 3.8-liter twin-turbo V-8, Horse Power: 1000HP, Top Speed: 217MPH'
+            },
+            {
+                id: 2,
+                name: 'Lykan HyperSport',
+                content: 'Price: $3.4 million, Engine: twin-turbo 3.8-liter flat-six boxer, Horse Power: 780HP, Top Speed: 240MPH '
+            },
+            {
+                id: 3,
+                name: 'Lamborghini Veneno Roadster',
+                content: 'Price: $3.3 million, Engine: 6.5-liter V-12, Horse Power: 740HP, Top Speed: 221MPH '
+            },
+            {
+                id: 4,
+                name: 'Aston Martin Valkyrie',
+                content: 'Price: $3 million, Engine: 6.5-liter, naturally-aspirated V-12, Horse Power: 1000HP, Top Speed: 250MPH '
+            },
+            {
+                id: 5,
+                name: 'Ferrari Pininfarina Sergio',
+                content: 'Price: $3 million, Engine: naturally-aspirated 4.5-liter F136F  V-8, Horse Power: 597HP, Top Speed: 202MPH'
+            },
+            {
+                id: 6,
+                name: 'Pagani Huayra BC',
+                content: 'Price: $2.8 million, Engine: 6.0-liter V-12 bi-turbo, Horse Power: 790HP, Top Speed: 238MPH'
+            },
+            {
+                id: 7,
+                name: 'Bugatti Chiron',
+                content: 'Price: $2.6 million, Engine: quad-turbocharged W-16 8.0-liter, Horse Power: 1500HP, Top Speed: 268MPH'
+            },
+            {
+                id: 8,
+                name: 'McLaren P1 GTR',
+                content: 'Price: $2.59 million, Engine: 3.8-liter V-8, Horse Power: 1000HP, Top Speed: 225MPH'
+            },
+            {
+                id: 9,
+                name: 'Ferrari LaFerrari Aperta',
+                content: 'Price: $2.2 million, Engine: 6.3-liter Ferrari F140 V12, Horse Power: 1000HP, Top Speed: 217MPH'
+            },
+            {
+                id: 10,
+                name: 'Koenigsegg Regera',
+                content: 'Price: $1.9 million, Engine: 5.0-liter twin-turbo V8, Horse Power: 1100HP, Top Speed: 255MPH'
+            }
         ];
         return { cars: cars };
     };
